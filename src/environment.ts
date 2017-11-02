ea0b72ceaa6a1e13d13bb51458e47d21d3657df1
Sopar Sihotang <soparsihotang@gmail.com>
2017-11-02 11:48:12 +0000

    New package tslint for validating ts & tsx files

---
+++ src/environment.ts
@@ -0,0 +1,13 @@
+/**
+ * @license
+ * Copyright (c) 2017, Sopar Sihotang. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/** * @internal */
+export const IS_DEV = process.env.NODE_ENV === "development";
+
+/** * @internal */
+export const IS_PROD = process.env.NODE_ENV === "production";
--- src/foundation/index.ts
+++
@@ -1,1 +0,0 @@
-export { Playst } from './Playst'

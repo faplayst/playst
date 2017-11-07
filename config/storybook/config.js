b879095db027e736adb9cceba6ae07d9af9d956f
Sopar Sihotang <soparsihotang@gmail.com>
2017-11-07 03:38:21 +0000

    Add script start for starting storybook

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,9 +1,7 @@
-import {
-  configure
-} from '@storybook/react';
+import { configure } from '@storybook/react';
 
-// const context = require.context('../../src/Playst', true, /stories\/.*\.story\.js$/);
-const context = require.context('../../src/Playst', true, /\.story\.js$/);
+const context = require.context('../../src/Playst', true, /\.stories\/.*\.story\.js$/);
+// const context = require.context('../../src/Playst', true, /\.story\.js$/);
 
 function loadStories() {
   context.keys().forEach(context);

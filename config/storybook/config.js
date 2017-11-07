1f5a8ac455c8d3d1e2c52cc2e0176250330675de
Sopar Sihotang <soparsihotang@gmail.com>
2017-11-07 07:29:35 +0000

    Setting up directories of storybook

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
-const context = require.context('../../src/Playst', true, /\.stories\/.*\.story\.js$/);
-// const context = require.context('../../src/Playst', true, /\.story\.js$/);
+// const context = require.context('../../src/Playst', true, /\/.*\.stories\/.*\.story\.js$/);
+const context = require.context('../../src', true, /.*_stories_\/.*\.story\.js$/);
 
 function loadStories() {
   context.keys().forEach(context);

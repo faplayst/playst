819ae97a43b1e991183e357c5c677447c905a220
Sopar Sihotang <soparsihotang@gmail.com>
2017-11-10 04:40:02 +0000

    Fixed directories of storybook

--- common/config/storybook/config.js
+++ common/config/storybook/config.js
@@ -1,7 +1,6 @@
 import { configure } from '@storybook/react';
 
-// const context = require.context('../../src/Playst', true, /\/.*\.stories\/.*\.story\.js$/);
-const context = require.context('../../../src', true, /.*_stories_\/.*\.story\.js$/);
+const context = require.context('../../../src', true, /.*__stories__\/.*\.story\.js$/);
 
 function loadStories() {
   context.keys().forEach(context);
